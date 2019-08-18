378f6e242202bbabbf0d64576b699df6068e9a3c
Eliza Munroe <era3munroe@gmail.com>
2019-08-18 13:06:47 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -12,6 +12,10 @@ class Patient
   def self.all
     @@all
   end
+  
+  def appointments
+    Appointment.all.select {|appointment| appointment.patient}
+  end
 
   def new_appointment(date, doctor)
     Appointment.new(date, self, doctor)
